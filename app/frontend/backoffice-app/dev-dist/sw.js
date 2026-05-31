a8c01aa331e118902ff9c01ae8f4cb39f31ba76c
MEHDImp4 <83414196+MEHDImp4@users.noreply.github.com>
2026-05-31 18:13:52 +0100

    style: final UI adjustments and centering fixes

--- app/frontend/backoffice-app/dev-dist/sw.js
+++ app/frontend/backoffice-app/dev-dist/sw.js
@@ -78,7 +78,7 @@ define(['./workbox-25613826'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.sspq9esoc4"
+    "revision": "0.dpcma8g6gk8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
